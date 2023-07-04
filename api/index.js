af9ba65cba6b13855cba13d7b8968a721d9752c3
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-07-04 22:06:50 +0530

    added cors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 4040;
+
+app.use(cors());
+app.use(express.json());
 app.get("/api/test", (req, res) => {
   res.json("test ok2");
 });
